45b0c7eaae2b0a196eadac8f85b3354ffeee1e87
ako <a.veysinejad@gmail.com>
2024-08-09 10:12:34 +0330

    fix: testing fix on deployment

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -7,7 +7,6 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  // output: "standalone",
   experimental: {
     // this includes files from the monorepo base two directories up
     outputFileTracingRoot: path.join(__dirname, "../../"),
